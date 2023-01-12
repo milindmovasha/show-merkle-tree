ae21ef63662e6538d16eab2703aa9794c689f81f
Milind Movasha <milind.movasha@gmail.com>
2023-01-12 19:39:09 +0530

    added env variable to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     reactStrictMode: true,
     distDir: "build",
+    env: {
+        NEXT_PUBLIC_WEB3_API_URL: process.env.NEXT_PUBLIC_WEB3_API_URL,
+    },
 }
 
 module.exports = nextConfig
